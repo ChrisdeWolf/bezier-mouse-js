ea7020f62bd4bcd6445efac6d46ed6e12b673890
Chris deWolf <cpdewolf@gmail.com>
2023-08-25 17:03:51 -0600

    try to host docs on github pages

--- docs/example.js
+++ docs/example.js
@@ -1,15 +1,14 @@
 import BezierMouse from "../src/BezierMouse.js";
 
-console.log({ BezierMouse });
 const BezMouse = new BezierMouse();
+
+const initPos = [100, 100];
+const finPos = [600, 600];
+const deviation = 20;
+const curve = BezMouse.cubicBezierCurve(initPos, finPos, deviation);
 
 let cvs = document.getElementById("CurveCanvas");
 let context = cvs.getContext("2d");
-const initPos = [100, 100];
-const finPos = [600, 600];
-const deviation = 5;
-
-const curve = BezMouse.cubicBezierCurve(initPos, finPos, deviation);
 context.lineWidth = 3;
 context.beginPath();
 const [init, control0, control1, fin] = curve.points;
