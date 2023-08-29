81204904559f3a184ed9dd1f599ade97296537d4
Chris deWolf <cpdewolf@gmail.com>
2023-08-28 22:13:31 -0600

    added initial mouse movement functionality

--- src/BezierMouse.js
+++ src/BezierMouse.js
@@ -9,15 +9,15 @@ import Utils from "./Utils.js";
 
 const { abs, ceil } = Math;
 
-mouse.config.mouseSpeed = 40;
+mouse.config.mouseSpeed = 100;
 
 /**
  * BezierMouse class generates natural mouse movement using bezier curves
  */
 export default class BezierMouse {
-  //   constructor(opts) {
-  //     // TODO
-  //   }
+  // constructor(opts) {
+  //   this.Bezier = Bezier;
+  // }
 
   /**
    * @typedef {Object} Point
@@ -66,4 +66,53 @@ export default class BezierMouse {
       initY + Utils.choice([-1, 1]) * deltaY * 0.01 * randDeviation(),
     ];
   }
+
+  async move(point) {
+    const p0 = new Point(100, 100);
+    const p1 = new Point(500, 500);
+    // TODO: need to copy what this does
+    //  - see https://github.com/nut-tree/nut.js/blob/develop/lib/movement.function.ts
+    //  - uses a linehelper class: https://github.com/nut-tree/nut.js/blob/develop/lib/util/linehelper.class.ts
+    //  - this is where we will use our
+    // console.log({ straightTo: await straightTo(p1) });
+    await mouse.move(this.bezierCurveTo(p0, p1)); //
+
+    // await mouse.move([p0, p1]);
+  }
+
+  async bezierCurveTo(origin, target) {
+    const curve = this.cubicBezierCurve([100, 100], [700, 700], 20);
+    const steps = 120; // https://pomax.github.io/bezierjs/#getLUT
+    const LUT = curve.getLUT(steps);
+    console.log({ points: curve.points, LUT });
+
+    // TODO: figure out why bezier is backwards!
+    // return LUT.map((point) => new Point(point.x, point.y));
+    let points = [];
+    for (let t = LUT.length - 1; t >= 0; t--) {
+      points.push(new Point(LUT[t].x, LUT[t].y));
+    }
+    return points;
+  }
+
+  // TODO:
+  //   * @param {number} speed - integer multiplier for speed. 1=fastest, 1< slower
+  // Calculate time parameter values
+  // const ts = [];
+  // for (let t = 0; t < speed * 101; t++) {
+  //   ts.push(t / (speed * 100));
+  // }
+
+  //   toString() {
+  //     return utils.pointsToString(this.points);
+  //   }
+
+  //   _error(pc, np1, s, e) {
+  //     //
+  //   }
 }
+
+(async () => {
+  const bezMouse = new BezierMouse();
+  await bezMouse.move();
+})();
