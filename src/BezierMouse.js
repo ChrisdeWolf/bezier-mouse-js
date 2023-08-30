b100bd33f4fcb7cd9c355755a8889b2398359506
Chris deWolf <cpdewolf@gmail.com>
2023-08-29 22:01:12 -0600

    add variable mouseSpeed and some cleanup

--- src/BezierMouse.js
+++ src/BezierMouse.js
@@ -4,20 +4,22 @@
   This code is MIT licensed.
 **/
 import { Bezier } from "bezier-js";
-import { Point, mouse, straightTo } from "@nut-tree/nut-js";
+import { Point, mouse } from "@nut-tree/nut-js";
 import Utils from "./Utils.js";
 
 const { abs, ceil } = Math;
-
-mouse.config.mouseSpeed = 100;
 
 /**
  * BezierMouse class generates natural mouse movement using bezier curves
  */
 export default class BezierMouse {
-  // constructor(opts) {
-  //   this.Bezier = Bezier;
-  // }
+  /**
+   * constructor
+   * @param {number} mouseSpeed - mouse movement speed in pixels per second
+   */
+  constructor(mouseSpeed) {
+    mouse.config.mouseSpeed = mouseSpeed || 100;
+  }
 
   /**
    * @typedef {Object} Point
@@ -27,7 +29,41 @@ export default class BezierMouse {
    * @param {Point} finPos
    * @param {number} deviation - deviation scale (larger = more curve)
    */
-  cubicBezierCurve(initPos, finPos, deviation, speed) {
+  async move(initPos, finPos, deviation) {
+    await mouse.move(this.bezierCurveTo(initPos, finPos, deviation));
+  }
+
+  /**
+   * @typedef {Object} Point
+   * @property {number} x - The x-coordinate of the point.
+   * @property {number} y - The y-coordinate of the point.
+   * @param {Point} initPos
+   * @param {Point} finPos
+   * @param {number} deviation - deviation scale (larger = more curve)
+   */
+  async bezierCurveTo(initPos, finPos, deviation) {
+    const curve = this.cubicBezierCurve(initPos, finPos, deviation);
+    const steps = 120; // TODO: handle step: https://pomax.github.io/bezierjs/#getLUT
+    const LUT = curve.getLUT(steps);
+
+    // TODO: figure out why bezier is backwards!
+    // return LUT.map((point) => new Point(point.x, point.y));
+    let points = [];
+    for (let t = LUT.length - 1; t >= 0; t--) {
+      points.push(new Point(LUT[t].x, LUT[t].y));
+    }
+    return points;
+  }
+
+  /**
+   * @typedef {Object} Point
+   * @property {number} x - The x-coordinate of the point.
+   * @property {number} y - The y-coordinate of the point.
+   * @param {Point} initPos
+   * @param {Point} finPos
+   * @param {number} deviation - deviation scale (larger = more curve)
+   */
+  cubicBezierCurve(initPos, finPos, deviation) {
     const [c0x, c0y] = this.getBezierControlPoint(initPos, finPos, deviation);
     const [c1x, c1y] = this.getBezierControlPoint(initPos, finPos, deviation);
     const [initX, initY] = initPos;
@@ -39,19 +75,12 @@ export default class BezierMouse {
       { x: c1x, y: c1y },
       { x: finX, y: finY },
     ];
-    const cubicCurve = new Bezier(cubicPoints);
-    return cubicCurve;
+    return new Bezier(cubicPoints);
   }
 
   /**
-   * returns a bezier control point between deviation/2 and
+   * returns a bezier control points between deviation/2 and
    * deviation of total distance (+/- random)
-   * @typedef {Object} Point
-   * @property {number} x - The x-coordinate of the point.
-   * @property {number} y - The y-coordinate of the point.
-   * @param {Point} initPos
-   * @param {Point} finPos
-   * @param {number} deviation - deviation scale (larger = more curve)
    */
   getBezierControlPoint(initPos, finPos, deviation = 5) {
     const [initX, initY] = initPos;
@@ -66,53 +95,4 @@ export default class BezierMouse {
       initY + Utils.choice([-1, 1]) * deltaY * 0.01 * randDeviation(),
     ];
   }
-
-  async move(point) {
-    const p0 = new Point(100, 100);
-    const p1 = new Point(500, 500);
-    // TODO: need to copy what this does
-    //  - see https://github.com/nut-tree/nut.js/blob/develop/lib/movement.function.ts
-    //  - uses a linehelper class: https://github.com/nut-tree/nut.js/blob/develop/lib/util/linehelper.class.ts
-    //  - this is where we will use our
-    // console.log({ straightTo: await straightTo(p1) });
-    await mouse.move(this.bezierCurveTo(p0, p1)); //
-
-    // await mouse.move([p0, p1]);
-  }
-
-  async bezierCurveTo(origin, target) {
-    const curve = this.cubicBezierCurve([100, 100], [700, 700], 20);
-    const steps = 120; // https://pomax.github.io/bezierjs/#getLUT
-    const LUT = curve.getLUT(steps);
-    console.log({ points: curve.points, LUT });
-
-    // TODO: figure out why bezier is backwards!
-    // return LUT.map((point) => new Point(point.x, point.y));
-    let points = [];
-    for (let t = LUT.length - 1; t >= 0; t--) {
-      points.push(new Point(LUT[t].x, LUT[t].y));
-    }
-    return points;
-  }
-
-  // TODO:
-  //   * @param {number} speed - integer multiplier for speed. 1=fastest, 1< slower
-  // Calculate time parameter values
-  // const ts = [];
-  // for (let t = 0; t < speed * 101; t++) {
-  //   ts.push(t / (speed * 100));
-  // }
-
-  //   toString() {
-  //     return utils.pointsToString(this.points);
-  //   }
-
-  //   _error(pc, np1, s, e) {
-  //     //
-  //   }
 }
-
-(async () => {
-  const bezMouse = new BezierMouse();
-  await bezMouse.move();
-})();
